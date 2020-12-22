5e20793176a76310ef6b87fc1f20cc1f1045e04d
rahathosen <rahathosen.me@gmail.com>
2020-12-22 16:54:33 +0600

    added some new function

--- Lab Task1/01_Basic Calculator/src/App.java
+++ Lab Task1/01_Basic Calculator/src/App.java
@@ -52,7 +52,6 @@ public class App {
             x = Math.sqrt(a);
         }
         return x;
-
     }
 
     public static void main(String[] args) throws Exception {
@@ -96,7 +95,6 @@ public class App {
                 System.out.println("Thank You for My Service :)");
                 System.exit(0);
             }
-
         } while (n != 7);
     }
 }
