26204363aa47552c0461ad4de9996f1aa2b80c5a
rahathosen <rahathosen.me@gmail.com>
2020-12-22 16:46:51 +0600

    added two more function

--- Lab Task1/01_Basic Calculator/src/App.java
+++ Lab Task1/01_Basic Calculator/src/App.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.text.DecimalFormat;
 
 public class App {
     public static double Addition(double a, double b) {
@@ -19,23 +20,40 @@ public class App {
     }
 
     public static double Modulo(double a, double b) {
+
         return a % b;
     }
 
-    // public static double Root(double a, double b) {
-    // System.out.println("Number 1 or Number 2");
-    // Scanner scanner = new Scanner(System.in);
-    // int n = scanner.nextInt();
-    // switch (n) {
-    // case 1:
-    // return Math.sqrt(a);
-    // case 2:
-    // return Math.sqrt(b);
-    // default:
-    // break;
-    // }
-    // return 0;
-    // }
+    public static double Square(double a, double b) {
+
+        System.out.println("Which Square ? Number 1 or Number 2");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double x = 0.0;
+        if (n == 1) {
+            x = (a * a);
+        }
+        if (n == 2) {
+            x = (b * b);
+        }
+        return x;
+    }
+
+    public static double Root(double a, double b) {
+        System.out.println("Which Square root? Number 1 or Number 2");
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double x = 0.0;
+        if (n == 1) {
+            x = Math.sqrt(a);
+        }
+        if (n == 2) {
+            x = Math.sqrt(a);
+        }
+        return x;
+
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
@@ -45,7 +63,7 @@ public class App {
             double number_1 = scanner.nextDouble();
             System.out.print("Enter your Second value: ");
             double number_2 = scanner.nextDouble();
-            System.out.println("Select Your Operation:\n1 = +\n2 = -\n3 = *\n4 = /\n5 = %");
+            System.out.println("Select Your Operation:\n1 = +\n2 = -\n3 = *\n4 = /\n5 = %\n6 = Square\n7 = SquareRoot");
             n = scanner.nextInt();
             switch (n) {
                 case 1:
@@ -63,12 +81,18 @@ public class App {
                 case 5:
                     System.out.println(Modulo(number_1, number_2));
                     break;
+                case 6:
+                    System.out.printf("%.2f\n", Square(number_1, number_2));
+                    break;
+                case 7:
+                    System.out.printf("%.2f\n", Root(number_1, number_2));
+                    break;
                 default:
                     break;
             }
             System.out.println("Are you want to exit Press 7 If not Press any");
             int ex = scanner.nextInt();
-            if (ex == 7) {
+            if (ex == 8) {
                 System.out.println("Thank You for My Service :)");
                 System.exit(0);
             }
